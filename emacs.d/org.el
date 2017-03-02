20d39b3b65d91c20afb157e4a4b12b73ca0a7f3d
bepzi <dev.ben01189998819991197253@gmail.com>
2017-03-02 06:30:39 -0600

    Add agenda priority colors

--- emacs.d/org.el
+++ emacs.d/org.el
@@ -8,7 +8,7 @@
 (setq org-capture-templates
       '(("t" "Add a task to the TODO list." entry
          (file "~/documents/tasks.org")
-         "* TODO %?
+         "* TODO [#A] %?
 SCHEDULED: %t DEADLINE: %t")
         ("a" "Add an upcoming date to the calendar." entry
          (file "~/documents/dates.org")
@@ -18,11 +18,19 @@ SCHEDULED: %t DEADLINE: %t")
 (setq org-todo-keywords
       '((sequence "TODO" "IN-PROGRESS" "WAITING" "|" "DONE" "CANCELED")))
 
+;; Set colors for priorities
+(setq org-priority-faces '((?A . (:foreground "#F0DFAF" :weight bold))
+                           (?B . (:foreground "LightSteelBlue"))
+                           (?C . (:foreground "OliveDrab"))))
+
+;; Always open Agenda to replace the current window
+(setq org-agenda-window-setup (quote current-window))
+
 ;; Global keybindings
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-iswitchb)
 
-;; Directory for agenda files
+;; Directory for Agenda files
 (setq org-agenda-files '("~/documents/"))
