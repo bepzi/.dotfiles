74371f71502dc49c8503772b6302fb211773aa3c
bepzi <dev.ben01189998819991197253@gmail.com>
2017-03-28 13:06:08 -0500

    auto-fill paragraphs in text and org-mo

--- emacs.d/behavior.el
+++ emacs.d/behavior.el
@@ -6,3 +6,7 @@
 
 ;; Automatically download ELPA packages if not present
 (setq use-package-always-ensure t)
+
+;; Automatically fill paragraphs instead of doing M-q a whole bunch
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+(add-hook 'org-mode-hook 'turn-on-auto-fill)
