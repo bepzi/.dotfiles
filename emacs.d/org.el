655f7898bdc324b7b72810b01415cc0bc538d94c
bepzi <dev.ben01189998819991197253@gmail.com>
2017-08-05 09:37:35 -0500

    Improve Org-mode as a day planner

--- emacs.d/org.el
+++ emacs.d/org.el
@@ -12,8 +12,10 @@
 (setq org-capture-templates
       '(("t" "Add a task to the TODO list." entry
          (file+headline "~/documents/agenda.org" "Tasks")
-         "* TODO %?
-SCHEDULED: %t DEADLINE: %t")
+         "* TODO %? %t")
+        ("n" "Add a note for later." entry
+         (file+headline "~/documents/agenda.org" "Notes")
+         "* TODO %? %t")
         ("a" "Add an upcoming date to the calendar." entry
          (file+headline "~/documents/agenda.org" "Calendar")
          "* %? %t")
@@ -23,23 +25,34 @@ SCHEDULED: %t DEADLINE: %t")
 
 ;; Default task sequence
 (setq org-todo-keywords
-      '((sequence "TODO" "|" "DONE" "CANCELED")))
+      '((sequence "TODO" "IN-PROGRESS" "WAITING" "|" "DONE" "DEFERRED" "CANCELED")))
 
 ;; Set priority range from A to C with default A
 (setq org-highest-priority ?A)
 (setq org-lowest-priority ?C)
-(setq org-default-priority ?A)
-
-;; Set colors for priorities
-(setq org-priority-faces '((?A . (:foreground "#F0DFAF" :weight bold))
-                           (?B . (:foreground "LightSteelBlue"))
-                           (?C . (:foreground "OliveDrab"))))
+(setq org-default-priority ?B)
 
 ;; Always open Agenda to replace the current window
 (setq org-agenda-window-setup (quote current-window))
 
+;; How many days ahead the agenda view should look
+(setq org-agenda-ndays 7)
+
+;; How many days early until a deadline item starts appearing
+(setq org-deadline-warning-days 14)
+
+;; Show days that don't have anything scheduled on them
+(setq org-agenda-show-all-dates t)
+
 ;; Don't show tasks as scheduled if they are already shown as a deadline
 (setq org-agenda-skip-scheduled-if-deadline-is-shown t)
+
+;; Hide deadline/scheduled tasks if they're done
+(setq org-agenda-skip-deadline-if-done t)
+(setq org-agenda-skip-scheduled-if-done t)
+
+;; Begin the agenda view on the current day, not Sunday
+(setq org-agenda-start-on-weekday nil)
 
 ;; Sort tasks in order of when they are due and then by priority
 (setq org-agenda-sorting-strategy
