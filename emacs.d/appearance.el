47c3750fe896eb84c79f302e254e67917fd5bad2
bepzi <dev.ben01189998819991197253@gmail.com>
2018-05-05 12:58:16 -0500

    Add whitespace-mode configs

--- emacs.d/appearance.el
+++ emacs.d/appearance.el
@@ -7,6 +7,45 @@
 ;; Enable line numbers
 (global-linum-mode t)
 
+;; Whitespace mode config
+;; Sourced from: https://github.com/rexim/dotfiles/blob/master/.emacs.rc/whitespace-mode-rc.el
+(defun rc/turn-on-whitespace-mode ()
+  (interactive)
+  (whitespace-mode 1))
+
+(defun rc/delete-trailing-whitespace-on-save ()
+  (interactive)
+  (add-to-list 'write-file-functions 'delete-trailing-whitespace))
+
+(defun rc/set-up-whitespace-handling ()
+  (interactive)
+  (rc/turn-on-whitespace-mode)
+  (rc/delete-trailing-whitespace-on-save))
+
+(setq whitespace-style '(space-mark tab-mark))
+
+(let ((whitespace-enabled-modes
+       '(
+         tuareg-mode-hook
+         c++-mode-hook
+         c-mode-hook
+         rust-mode-hook
+         emacs-lisp-mode
+         java-mode-hook
+         lua-mode-hook
+         rust-mode-hook
+         scala-mode-hook
+         markdown-mode-hook
+         js2-mode-hook
+         haskell-mode-hook
+         python-mode-hook
+         erlang-mode-hook
+         asm-mode-hook
+         nasm-mode-hook
+         )))
+  (dolist (mode whitespace-enabled-modes)
+(add-hook mode 'rc/set-up-whitespace-handling)))
+
 ;; Custom font size
 ;; Height value is in 1/10pt, so 100=10pt
 ;; (set-face-attribute 'default nil :height 110)
