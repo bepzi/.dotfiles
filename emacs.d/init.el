4f5aec14e4ece76ccf4ae6ab8bacce9c3154002e
bepzi <dev.ben01189998819991197253@gmail.com>
2018-11-16 10:54:02 -0600

    Rewrite Emacs config to be literate
    Closes: #18, #21, #27, #28

---
+++ emacs.d/init.el
@@ -0,0 +1,12 @@
+;;; init.el --- Entry point for Emacs configuration
+;;; Commentary:
+;;
+;; Loads the literate config file, then runs all of the included
+;; elisp.
+
+;;; Code:
+
+(org-babel-load-file "~/.emacs.d/emacs-config.org")
+
+;;; init.el ends here
+
