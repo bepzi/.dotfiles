edbe5ce15d78da5b630159fe9bd377838c05a3c4
bepzi <dev.ben01189998819991197253@gmail.com>
2019-01-12 08:20:14 -0600

    Specify symlink behavior to prevent issues when reading the config file

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -6,7 +6,10 @@
 
 ;;; Code:
 
+;; Automatically follow symlinks without prompting
+;; Necessary since we use symlinks to manage our config files
+(setq vc-follow-symlinks t)
+
 (org-babel-load-file "~/.emacs.d/emacs-config.org")
 
 ;;; init.el ends here
-
